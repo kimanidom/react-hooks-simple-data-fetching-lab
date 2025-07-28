0cd5ba300371b740e6c6eb17fafeb28b176e972e
Dominic Kimani <kimanidominic71@gmail.com>
2025-07-28 13:03:47 +0300

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,26 @@
 // create your App component here
+import React, { useState, useEffect } from "react";
+
+function App() {
+  const [imageUrl, setImageUrl] = useState(null);
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((res) => res.json())
+      .then((data) => {
+        setImageUrl(data.message); // The API returns the image URL under `message`
+      });
+  }, []);
+
+  if (!imageUrl) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <div>
+      <img src={imageUrl} alt="A Random Dog" />
+    </div>
+  );
+}
+
+export default App;
